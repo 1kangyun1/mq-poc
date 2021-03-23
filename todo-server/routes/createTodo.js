f8d48fabcdc8699ffdfbd5c0a5cd9efe4843d346
1kangyun1 <1kangyun1@gmail.com>
2021-03-23 18:56:56 -0400

    implemented create and delete todo

--- todo-server/routes/createTodo.js
+++ todo-server/routes/createTodo.js
@@ -5,23 +5,9 @@ const { v4: uuidv4 } = require('uuid');
 var router = express.Router();
 
 router.post('/', async(req, res) => {
-  try {
-    const pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
-
-    const client = await pool.connect();
-    const result = await client.query('INSERT INTO todos VALUES ($1, $2, $3, $4) RETURNING *;', 
-                                          [uuidv4(), req.body.todo, req.body.finished, Date.now()]);
-    res.json( result.rows[0] );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
+  let newTodo = {id:uuidv4(), todo:req.body.todo, finished:req.body.finished, time:Date.now()};
+  res.locals.DB.todos.push(newTodo);
+  res.json(newTodo);
 });
 
 module.exports = router;
--- todo-server/routes/delTodo.js
+++ todo-server/routes/delTodo.js
@@ -4,21 +4,10 @@ const { Pool } = require('pg');
 var router = express.Router();
 
 router.delete('/:id',  async(req, res) => {
-  try {
-    const pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
-
-    const client = await pool.connect();
-    const result = await client.query('DELETE FROM todos WHERE id = $1 RETURNING *;', [req.params.id]);
-    res.json( result.rows );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
+  for(let index=0; index<res.locals.DB.todos.length; index++){
+    if(res.locals.DB.todos[index].id === req.params.id){
+      res.json(res.locals.DB.todos.splice(index, 1));
+    }
   }
 });
 
